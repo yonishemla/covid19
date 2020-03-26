d78d6585f4037329afc8ea2636709fde3c05f7ce
יונתן שמלה <yonishemla@gmail.com>
2020-03-26 16:38:46 +0200

    second

--- api/routes/people.js
+++ api/routes/people.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const router = express.Router();
 
@@ -14,22 +15,21 @@ const connection = mysql.createConnection({
 router.post('/', (req,res,next)=>{
 
     const logs = {
-        longitude: req.body.longitude,
+        longitude: req.body.longitude, 
         latitude: req.body.latitude,
-        people_id: req.body.people_id,
-        date: req.body.date,
-        rssi: req.body.rssi,
-        user_id: req.body.user_id,
-        battery_status: req.body.battery_status
+        people_id: req.body.people_id, 
+        date: req.body.date, 
+        user_id: req.body.user_id 
     };
+
+
     res.status(201).json({
         message: 'Handeling POST requests to /logs',
         foundPeople: logs
     });
 
 
-    var sql = "INSERT INTO logs (longitude,latitude,people_id,date,rssi,user_id,battery_status) VALUES ("
-    +req.body.longitude+","+req.body.latitude+",'"+req.body.people_id+"','"+req.body.date+"','"+req.body.rssi+"','"+req.body.user_id+"',"+req.body.battery_status+")";
+    var sql = "INSERT INTO logs (longitude,latitude,people_id,date,user_id) VALUES ("+req.body.longitude+","+req.body.latitude+",'"+req.body.people_id+"','"+req.body.date+"','"+req.body.user_id+"')";
 
     console.log(sql);
     connection.query(sql, function (error, results) {
